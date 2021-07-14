99a35640e9248c3bb23e3021e4a9fdb01b563451
Subiya <subiya2102@gmail.com>
2021-07-14 13:38:07 +0530

    Initail Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from plyer import notification
+ 
+while True:
+  time.sleep(900)
+  notification.notify(
+ 			title = "Hey User!! \n**Its  time to give rest to your eyes ",
+ 			message ="Please close your atleast for 10 seconds.By doing this you can avoid many sevior issues related to your eyes",
+ 			app_icon = r"C:\Users\subiy\OneDrive\Desktop\python practice\eye.ico",
+ 			timeout= 10
+ 			)
+  time.sleep(10)
+  time.sleep(900)
+  notification.notify(
+ 			title = "Hey User!! \n**Its time to take a healthy walk.",
+ 			message ="Taking a healthy walk for 5 minutes can reduce sevior phsycal issues.",
+ 			app_icon = r"C:\Users\subiy\OneDrive\Desktop\python practice\alk.ico",
+ 			timeout= 10
+ 			)
+  time.sleep(300)
+  time.sleep(900)
+  notification.notify(
+ 			title = "Hey User!! \n **Its time to give rest to your eyes ",
+ 			message ="Please close your atleast for 10 seconds.By doing this you can avoid many sevior issues related to your eyes",
+ 			app_icon = r"C:\Users\subiy\OneDrive\Desktop\python practice\eye.ico",
+ 			timeout= 10
+ 			)
+  time.sleep(10)
+  time.sleep(900)
+  notification.notify(
+ 			title = "Hey User!!\n**Please Drink Water Now!!",
+ 			message ="The National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+ 			app_icon = r"C:\Users\subiy\OneDrive\Desktop\python practice\icon.ico",
+ 			timeout= 10
+ 			)
